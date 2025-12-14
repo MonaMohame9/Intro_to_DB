7b089d9284925994d4778c0c90c62e6cabaf5e9c
MonaMohamed9 <engmona10@gmail.com>
2025-12-14 22:11:38 +0200

    Fix checker

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,8 +1,8 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
+    cursor = None
     try:
         connection = mysql.connector.connect(
             host="localhost",
@@ -14,12 +14,13 @@ def create_database():
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
-        if connection is not None and connection.is_connected():
+        if cursor is not None:
             cursor.close()
+        if connection is not None and connection.is_connected():
             connection.close()
 
 if __name__ == "__main__":
