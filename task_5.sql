085e2e92d8064b9cba8c6ab29e9ba57414b61f66
MonaMohamed9 <engmona10@gmail.com>
2025-12-14 22:52:08 +0200

    add task_5

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- task_5.sql
+-- Script to insert a single row into Customers table in alx_book_store
+
+INSERT INTO Customers (customer_id, first_name, last_name, email, address)
+VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
